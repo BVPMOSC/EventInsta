28fa31a2a38336cf02ba912b5b7cea32b9cbb976
aniket965 <aniket965.as@gmail.com>
2017-08-02 23:23:52 +0530

    theme added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,42 @@ import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBarExampleIcon from './Components/AppBarExampleIcon'
+import {
+  teal500, teal700,
+  pinkA200,
+  grey100, grey300, grey400, grey500,
+  white, darkBlack, fullBlack,
+} from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
 injectTapEventPlugin();
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: teal500,
+    primary2Color: teal700,
+    primary3Color: grey400,
+    accent1Color: pinkA200,
+    accent2Color: grey100,
+    accent3Color: grey500,
+    textColor: darkBlack,
+    alternateTextColor: white,
+    canvasColor: white,
+    borderColor: grey300,
+    pickerHeaderColor: teal500,
+    shadowColor: fullBlack,
+  },
+  appBar: {
+    height: 60,
+  },
+});
+
+
 class App extends Component {
 
   render() {
     return (
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={muiTheme}>
         <AppBarExampleIcon />
       </MuiThemeProvider>
     );
