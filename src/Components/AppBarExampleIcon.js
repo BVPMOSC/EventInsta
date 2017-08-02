1d95a5b7ee9a4202d223808c9f771b4e01343a4b
aniket965 <aniket965.as@gmail.com>
2017-08-03 02:03:07 +0530

    added profile avatar in drawer

--- src/Components/AppBarExampleIcon.js
+++ src/Components/AppBarExampleIcon.js
@@ -7,9 +7,10 @@ import FontIcon from 'material-ui/FontIcon';
 import MdLabel from 'react-icons/lib/md/label'
 import MdExitToApp from 'react-icons/lib/md/exit-to-app'
 import MdEvent from 'react-icons/lib/md/event'
-
-
-
+import IconButton from 'material-ui/IconButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import ListItem from 'material-ui/List/ListItem';
+import Avatar from 'material-ui/Avatar';
 
 const iconStyles = {
   marginRight: 24,
@@ -41,10 +42,22 @@ export default class AppBarExampleIcon extends React.Component {
           open={this.state.open}
           onRequestChange={(open) => this.setState({ open })}
         >
+
+          <AppBar title="" iconElementLeft={<IconButton><NavigationClose /></IconButton>} onLeftIconButtonTouchTap={this.handleClose}	/>
           <Divider />
-          <MenuItem onTouchTap={this.handleClose}><MdEvent style={iconStyles}/>Latest Events</MenuItem>
-          <MenuItem onTouchTap={this.handleClose}> <MdLabel style={iconStyles}/>Societies Tags</MenuItem>
-          <MenuItem onTouchTap={this.handleClose}><MdExitToApp style={iconStyles}/>Logout</MenuItem>
+          <MenuItem onTouchTap={this.handleClose}>
+            <ListItem
+              disabled={true}
+              leftAvatar={
+                <Avatar src="images/avatar.jpg" />
+              }>Aniket965
+            </ListItem>
+            <Divider />
+          </MenuItem>
+
+          <MenuItem onTouchTap={this.handleClose}><MdEvent style={iconStyles} />Latest Events</MenuItem>
+          <MenuItem onTouchTap={this.handleClose}> <MdLabel style={iconStyles} />Societies Tags</MenuItem>
+          <MenuItem onTouchTap={this.handleClose}><MdExitToApp style={iconStyles} />Logout</MenuItem>
         </Drawer>
       </div>
     );
