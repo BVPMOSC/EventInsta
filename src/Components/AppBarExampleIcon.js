9678c66b04345b48814edc5e32d66298f145accb
aniket965 <aniket965.as@gmail.com>
2017-08-03 01:44:31 +0530

    icons added to drawer

--- src/Components/AppBarExampleIcon.js
+++ src/Components/AppBarExampleIcon.js
@@ -1,40 +1,50 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
+import Divider from 'material-ui/Divider';
 import MenuItem from 'material-ui/MenuItem';
-/**
- * A simple example of `AppBar` with an icon on the right.
- * By default, the left icon is a navigation-menu.
- */
+import FontIcon from 'material-ui/FontIcon';
+import MdLabel from 'react-icons/lib/md/label'
+import MdExitToApp from 'react-icons/lib/md/exit-to-app'
+import MdEvent from 'react-icons/lib/md/event'
+
+
+
+
+const iconStyles = {
+  marginRight: 24,
+};
 
 
 export default class AppBarExampleIcon extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {open: false};
+    this.state = { open: false };
   }
 
-  handleToggle = () => this.setState({open: !this.state.open});
+  handleToggle = () => this.setState({ open: !this.state.open });
 
-  handleClose = () => this.setState({open: false});
+  handleClose = () => this.setState({ open: false });
 
   render() {
     return (
       <div>
-  <AppBar
-    title="Eveinsta"
-    iconClassNameRight="muidocs-icon-navigation-expand-more"
-    onLeftIconButtonTouchTap={this.handleToggle}
-  />
+        <AppBar
+          title="Eveinsta"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+          onLeftIconButtonTouchTap={this.handleToggle}
+        />
         <Drawer
           docked={false}
-          width={200}
+          width={260}
           open={this.state.open}
-          onRequestChange={(open) => this.setState({open})}
+          onRequestChange={(open) => this.setState({ open })}
         >
-          <MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
-          <MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
+          <Divider />
+          <MenuItem onTouchTap={this.handleClose}><MdEvent style={iconStyles}/>Latest Events</MenuItem>
+          <MenuItem onTouchTap={this.handleClose}> <MdLabel style={iconStyles}/>Societies Tags</MenuItem>
+          <MenuItem onTouchTap={this.handleClose}><MdExitToApp style={iconStyles}/>Logout</MenuItem>
         </Drawer>
       </div>
     );
