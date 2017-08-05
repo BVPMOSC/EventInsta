bba1f7bf545ad1340ea2ce9f515901bcb8aac88c
aniket965 <aniket965.as@gmail.com>
2017-08-05 22:26:51 +0530

    updated socities tags

--- src/Components/TagsPage.js
+++ src/Components/TagsPage.js
@@ -1,7 +1,70 @@
 import React, { Component } from 'react'
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import SocityCard from './SocityCard';
+import { ref } from '../config/constants'
+import { Card, CardActions, CardHeader, CardMedia, CardTitle } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import RssIcon from 'react-icons/lib/fa/feed';
+import CloseIcon from 'react-icons/lib/fa/close'
+function SocietyList(props) {
+    const socities = props.socities;
+    const listItems = socities.map((socity) =>
+        <Col xs={12} sm={4} md={3} lg={3}>
+            <Card>
+                <CardHeader
+                    title={socity.name}
+                    subtitle={socity.subtitle}
+                    avatar={socity.image}
+                />
+                <CardMedia>
+                    <img src={socity.poster_url} alt="" />
+                </CardMedia>
+                <CardActions>
+                    <FlatButton label={socity.following + " Following"} primary={true} icon={<RssIcon />} />
+                </CardActions>
+            </Card>
+            <h1></h1>
+        </Col>
+    );
+    return (
+        <div>{listItems}</div>
+    );
+}
 class TagsPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            socities: []
+        };
+
+    }
+
+    componentWillMount() {
+
+        var _this = this
+        this.ref = ref.child("/orgs")
+
+        this.ref.once('value', function (snapshot) {
+            var items = [];
+            snapshot.forEach(function (childSnapshot) {
+                var childKey = childSnapshot.key;
+                var childData = childSnapshot.val();
+                items.push(childData);
+                // ...
+            });
+
+            _this.setState({
+                socities: items
+            });
+
+        });
+    }
+
+
+    componentWillUnmount() {
+        this.ref.off();
+    }
+
+
     render() {
         return (
             <div>
@@ -17,39 +80,7 @@ class TagsPage extends Component {
                         </Col>
                     </Row>
                     <Row>
-                        <Col xs={12} sm={4} md={3} lg={3}>
-                            <SocityCard />
-                            <h1></h1>
-                        </Col>
-                         <Col xs={12} sm={4} md={3} lg={3}>
-                            <SocityCard />
-                            <h1></h1>
-                        </Col>
-                         <Col xs={12} sm={4} md={3} lg={3}>
-                            <SocityCard />
-                            <h1></h1>
-                        </Col>
-                         <Col xs={12} sm={4} md={3} lg={3}>
-                            <SocityCard />
-                            <h1></h1>
-                        </Col>
-                         <Col xs={12} sm={4} md={3} lg={3}>
-                            <SocityCard />
-                            <h1></h1>
-                        </Col>
-                         <Col xs={12} sm={4} md={3} lg={3}>
-                            <SocityCard />
-                            <h1></h1>
-                        </Col>
-                         <Col xs={12} sm={4} md={3} lg={3}>
-                            <SocityCard />
-                            <h1></h1>
-                        </Col>
-                         <Col xs={12} sm={4} md={3} lg={3}>
-                            <SocityCard />
-                            <h1></h1>
-                        </Col>
-
+                        <SocietyList socities={this.state.socities} />
                     </Row>
 
                 </Grid>
