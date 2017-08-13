ecdcaf84e2f2af26fa6ba13d252934762b8145b7
vaibhavchellani <vaibhavchellani223@gmail.com>
2017-08-13 21:40:45 +0530

    FCM done

--- app/firebase/functions/index.js
+++ app/firebase/functions/index.js
@@ -7,19 +7,51 @@ exports.sendNotifications = functions.database.ref('/PWA/events/{event_id}').onW
   // Only send a notification when a new message has been created.
   /*if (snapshot.previous.val()) {
     return;
+
   }*/
+  console.log(event.data);
 	console.log('data changed');// Notification details.
+  console.log(snapshot);
+  console.log(snapshot._newData);
   const text = "trial";
   const payload = {
     notification: {
-      title: text,
-      body: text ? (text.length <= 100 ? text : text.substring(0, 97) + '...') : '',
-      icon: 'icon.jpg',
+      title: snapshot._newData.event_heading,
+      body: 'Hey '+snapshot._newData.tag+' just added/modified their Event , have a look ! '
+      ,
+      icon:'https://avatars3.githubusercontent.com/u/1909340?v=4&s=200',
       click_action: `https://${functions.config().firebase.authDomain}`
     }
   };
+
+  admin.database().ref('/PWA/fcm_tokens').once('value', function(snapshot) {
+  
+  var registration_tokens=[];
+
+  snapshot.forEach(function(childSnapshot) {
+    var childData = childSnapshot.val();
+    //console.log(childData.token);
+    registration_tokens.push(childData.token);
+  });
+  console.log(registration_tokens);
+
+  admin.messaging().sendToDevice(registration_tokens, payload)
+  .then(function(response) {
+    // See the MessagingDevicesResponse reference documentation for
+    // the contents of response.
+    console.log("Successfully sent message:", response);
+  })
+  .catch(function(error) {
+    console.log("Error sending message:", error);
+  });
+
+
+});
+
+
+
 //change topic to tag as recieved from DB
-var topic="trial";
+/*var topic="trial";
   admin.messaging().sendToTopic(topic, payload)
   .then(function(response) {
     // See the MessagingTopicResponse reference documentation for the
@@ -29,14 +61,31 @@ var topic="trial";
   .catch(function(error) {
     console.log("Error sending message:", error);
   });
+*/
+
+
 
   // Get the list of device tokens.
-  /*return admin.database().ref('fcmTokens').once('value').then(allTokens => {
+  /*return admin.database().ref('/PWA/fcm_tokens').once('value').then(allTokens => {
     if (allTokens.val()) {
       // Listing all tokens.
+      //console.log(allTokens)
       const tokens = Object.keys(allTokens.val());
+      console.log(allTokens.val().tokens);
+      console.log(allTokens.val().val());
+      console.log("inside ref");
+      console.log(tokens);
+      console.log(tokens.token);
+      console.log(Object.val(allTokens.val()))
+      console.log("trying getvalue on tokens");
+      console.log(tokens.getvalue());
+            console.log("trying val on tokens");
 
+      console.log("trying values on tokens");
+
+      console.log(tokens.values());
       // Send notifications to all tokens.
+
       return admin.messaging().sendToDevice(tokens, payload).then(response => {
         // For each message check if there was an error.
         const tokensToRemove = [];
@@ -45,7 +94,7 @@ var topic="trial";
           if (error) {
             console.error('Failure sending notification to', tokens[index], error);
             // Cleanup the tokens who are not registered anymore.
-            if (error.code === 'messaging/invalid-registration-token' ||
+             if (error.code === 'messaging/invalid-registration-token' ||
                 error.code === 'messaging/registration-token-not-registered') {
               tokensToRemove.push(allTokens.ref.child(tokens[index]).remove());
             }
