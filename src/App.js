23ddc1d63f7faa1a319a373a890e57a02ecce12b
Piyush Agrawal <alltimepresent@gmail.com>
2017-09-01 01:20:27 +0530

    refactored code, removed material theme, added semantic theme, eslinter, login page updated

--- src/App.js
+++ src/App.js
@@ -1,136 +1,119 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AppBarDrawer from './Components/AppBarDrawer'
 import EventPage from './Components/EventPage'
 import {
   cyan500, cyan700,
   blue500,
   grey100, grey300, grey400, grey500,
-  white, darkBlack, fullBlack,
-} from 'material-ui/styles/colors';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
+  white, darkBlack, fullBlack
+} from 'material-ui/styles/colors'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import Footer from './Components/Footer'
-import { firebaseAuth } from './config/constants'
+import { firebaseAuth, ref } from './config/constants'
 import TagsPage from './Components/TagsPage'
 import LoginPage from './Components/LoginPage'
 import AddPage from './Components/AddPage'
-import { ref } from './config/constants'
-injectTapEventPlugin();
+// import 'semantic-ui-css/semantic.min.css'
+
+injectTapEventPlugin()
 
 const muiTheme = getMuiTheme({
-  palette: {
-    primary1Color: cyan500,
-    primary2Color: cyan700,
-    primary3Color: grey400,
-    accent1Color: blue500,
-    accent2Color: grey100,
-    accent3Color: grey500,
-    textColor: darkBlack,
-    alternateTextColor: white,
-    canvasColor: white,
-    borderColor: grey300,
-    pickerHeaderColor: cyan500,
-    shadowColor: fullBlack,
-  },
-  appBar: {
-    height: 60,
-  },
-});
-
+	palette: {
+		primary1Color: cyan500,
+		primary2Color: cyan700,
+		primary3Color: grey400,
+		accent1Color: blue500,
+		accent2Color: grey100,
+		accent3Color: grey500,
+		textColor: darkBlack,
+		alternateTextColor: white,
+		canvasColor: white,
+		borderColor: grey300,
+		pickerHeaderColor: cyan500,
+		shadowColor: fullBlack
+	},
+	appBar: {
+		height: 60
+	}
+})
 
 class App extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: false,
-      authed: false,
-      loading: true,
-      userName: "",
-      photoUrl: "",
-      events: null,
-      isAdmin: false,
-      useremail: ""
-    };
+	constructor (props) {
+		super(props)
+		this.state = {
+			open: false,
+			authed: false,
+			loading: true,
+			userName: '',
+			photoUrl: '',
+			events: null,
+			isAdmin: false,
+			useremail: ''
+		}
 
-
-
-    // we can change this after we decide on the structure of database 
-    firebaseAuth().onAuthStateChanged((user) => {
+    // we can change this after we decide on the structure of database
+		firebaseAuth().onAuthStateChanged((user) => {
       // console.log(user.photoURL)
-      if (user) {
+			if (user) {
+				this.setState({
+					userName: user.displayName,
+					photoUrl: user.photoURL,
+					useremail: user.email,
+					authed: true,
+					loading: false
+				})
+			}	else {
+				this.setState({
+					userName: '',
+					photoUrl: '',
+					authed: false,
+					loading: false
+				})
+			}
+		})
+	}
 
-        this.setState({
-          userName: user.displayName,
-          photoUrl: user.photoURL,
-          useremail: user.email,
-          authed: true,
-          loading: false,
-        });
-        
-  
-      }
-      else {
-        this.setState({
-          userName: "",
-          photoUrl: "",
-          authed: false,
-          loading: false
-        });
-      }
+	componentWillMount () {
+		var _this = this
+		var eventref = ref.child('/admins')
+		eventref.once('value', (snapshot) => {
+			snapshot.forEach(function (childSnapshot) {
+				var childData = childSnapshot.val()
+				if (childData.email === _this.state.useremail) {
+					console.log('done')
 
-    });
+					_this.setState({
+						isAdmin: true
+					})
+				}
+			})
+		})
+	}
+	componentWillUnmount () {
+		this.ref.off()
+	}
 
-
-
-  }
-
-  componentWillMount() {
-    var _this = this
-        var eventref = ref.child('/admins');
-        eventref.once('value', (snapshot) => {
-          snapshot.forEach(function (childSnapshot) {
-            var childData = childSnapshot.val();
-            if (childData.email === _this.state.useremail) {
-                console.log("done");
-
-              _this.setState({
-                isAdmin: true
-              });
-
-            }
-          });
-        })
-  }
-  componentWillUnmount() {
-    this.ref.off();
-  }
-
-  render() {
-
-    const AdminRoute = ({ component: Component, ...rest }) => (
+	render () {
+		const AdminRoute = ({ component: Component, ...rest }) => (
       <Route {...rest} render={props => (
         this.state.isAdmin ? (
           <Component {...props} />
         ) : (
             <Redirect to={{
-              pathname: '/',
-              state: { from: props.location }
-            }} />
+	pathname: '/',
+	state: { from: props.location }
+}} />
           )
-      )} />);
+      )} />)
 
-
-
-
-    return (
-
+		return (
 
       <Router>
         <MuiThemeProvider muiTheme={muiTheme}>
-          {this.state.authed  ? (
+          {this.state.authed ? (
             <div>
               <AppBarDrawer
                 authed={this.authed}
@@ -149,11 +132,8 @@ class App extends Component {
         </MuiThemeProvider>
       </Router>
 
-    );
-  }
-
+		)
+	}
 }
 
-
-
-export default App;
+export default App
--- src/Components/LoginPage/index.js
+++ src/Components/LoginPage/index.js
@@ -1,37 +1,102 @@
-import React, { Component } from 'react';
-import './Login.css';
-var firebase = require("firebase");
-var firebaseui = require('firebaseui');
+import React, { Component } from 'react'
+import { Button, Divider, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+
+var firebase = require('firebase')
+
+const docsButtonStyle = {
+	position: 'fixed',
+	margin: '2em',
+	bottom: 0,
+	left: 0,
+	animation: 'back-to-docs 1.5s ease-in-out infinite',
+	zIndex: 6
+}
+
+const style = (
+  <style>{`
+    @keyframes back-to-docs {
+        0% { transform: translateY(0); }
+        50% { transform: translateY(0.35em); }
+        100% { transform: translateY(0); }
+    }
+  `}</style>
+)
 
 export default class Login extends Component {
+	// noinspection SpellCheckingInspection
+	componentDidMount () {
+    //  Firebase config
+		var provider = new firebase.auth.GoogleAuthProvider()
+		firebase.auth().languageCode = 'en'
+	}
 
-  componentDidMount() {
-        // FirebaseUI config.
-    var uiConfig = {
-      signInSuccessUrl: '/',
-      signInOptions: [
-        // Leave the lines as is for the providers you want to offer your users.
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        firebase.auth.PhoneAuthProvider.PROVIDER_ID
-      ],
-      // Terms of service url.
-      tosUrl: ''
-    };
-  // Initialize the FirebaseUI Widget using Firebase.
-    var ui = new firebaseui.auth.AuthUI(firebase.auth());
-    // The start method will wait until the DOM is loaded.
-    ui.start('#firebaseui-auth-container', uiConfig);
+	render () {
+		return (
+      <div>
+        {style}
+        <div className='login-form'>
+          {/*
+      Heads up! The styles below are necessary for the correct render of this example.
+      You can do same with CSS, the main idea is that all the elements up to the `Grid`
+      below must have a height of 100%.
+    */}
+          <style>{`
+      body > div,
+      body > div > div,
+      body > div > div > div.login-form {
+        height: 100%;
+      }
+    `}</style>
+          <Grid
+            textAlign='center'
+            style={{ height: '100%' }}
+            verticalAlign='middle'
+          >
+            <Grid.Column style={{ maxWidth: 350 }}>
+              <Form size='large'>
+                <Segment stacked>
+                  <Header as='h2' color='teal' textAlign='center'>
+                    {' '}EventInsta
+                  </Header>
+                  <Divider />
+                  <Form.Input
+                    fluid
+                    icon='user'
+                    iconPosition='left'
+                    placeholder='E-mail address'
+                  />
+                  <Form.Input
+                    fluid
+                    icon='lock'
+                    iconPosition='left'
+                    placeholder='Password'
+                    type='password'
+                  />
+                  <Button color='teal' icon='mail outline' fluid size='medium' content='Signin with Email' />
+                  <br/>
+                  <Button color='google plus' icon='google plus' fluid size='medium' content='Sign in with Google'/>
+                </Segment>
+              </Form>
+              <Message>
+                <Button
+                  color='grey'
+                  content='Like'
+                  icon='heart'
+                />
 
-  }
-  render() {
-    return (
-      <div className="Home">
-        <div className="center">
-          <p className="login-text">Welcome to Eventinsta</p>
-          <div id="firebaseui-auth-container"></div>
+                <Button
+                  basic
+                  color='blue'
+                  content='Fork'
+                  icon='fork'
+                />
+              </Message>
+            </Grid.Column>
+          </Grid>
+        </div>
+        <div style={docsButtonStyle}>
         </div>
       </div>
-    );
-  }
+		)
+	}
 }
