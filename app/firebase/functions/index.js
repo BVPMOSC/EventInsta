94fe07695d2c546253127f84f63dd9be74a4f65b
vaibhavchellani <vaibhavchellani223@gmail.com>
2017-10-04 16:44:47 +0530

    some changes

--- app/firebase/functions/index.js
+++ app/firebase/functions/index.js
@@ -2,16 +2,12 @@ const functions = require('firebase-functions');
 var admin = require("firebase-admin");
 
 admin.initializeApp(functions.config().firebase);
+
+
+
 exports.sendNotifications = functions.database.ref('/PWA/events/{event_id}').onWrite(event => {
   const snapshot = event.data;
-  // Only send a notification when a new message has been created.
-  /*if (snapshot.previous.val()) {
-    return;
 
-  }*/
-  console.log(snapshot);
-
-  const text = "trial";
   const payload = {
     notification: {
       title: snapshot._newData.event_heading,
@@ -21,91 +17,58 @@ exports.sendNotifications = functions.database.ref('/PWA/events/{event_id}').onW
       click_action: `https://${functions.config().firebase.authDomain}`
     }
   };
-  if(snapshot._data==null){
 
-    admin.database().ref('/PWA/fcm_tokens').once('value', function(snapshot) {
+  //TODO see how to get topic from db here 
+  if(snapshot._data==null)
+  {
 
-      var registration_tokens=[];
-
-      snapshot.forEach(function(childSnapshot) {
-        var childData = childSnapshot.val();
-    //console.log(childData.token);
-    registration_tokens.push(childData.token);
-      });
-      console.log(registration_tokens);
-
-      admin.messaging().sendToDevice(registration_tokens, payload)
-      .then(function(response) {
-    // See the MessagingDevicesResponse reference documentation for
-    // the contents of response.
-    console.log("Successfully sent message:", response);
-  })
-      .catch(function(error) {
-        console.log("Error sending message:", error);
-      });
-
-
-    });
-
-
+    admin.messaging().sendToTopic(topic, payload)
+    .then(function(response) {
+      console.log("Successfully sent message:", response);
+    })
+    .catch(function(error) {
+      console.log("Error sending message:", error);
+    });    
 
   }
-  
-
-
-
-//change topic to tag as recieved from DB
-/*var topic="trial";
-  admin.messaging().sendToTopic(topic, payload)
-  .then(function(response) {
-    // See the MessagingTopicResponse reference documentation for the
-    // contents of response.
-    console.log("Successfully sent message:", response);
-  })
-  .catch(function(error) {
-    console.log("Error sending message:", error);
-  });
-  */
-
-
-
-  // Get the list of device tokens.
-  /*return admin.database().ref('/PWA/fcm_tokens').once('value').then(allTokens => {
-    if (allTokens.val()) {
-      // Listing all tokens.
-      //console.log(allTokens)
-      const tokens = Object.keys(allTokens.val());
-      console.log(allTokens.val().tokens);
-      console.log(allTokens.val().val());
-      console.log("inside ref");
-      console.log(tokens);
-      console.log(tokens.token);
-      console.log(Object.val(allTokens.val()))
-      console.log("trying getvalue on tokens");
-      console.log(tokens.getvalue());
-            console.log("trying val on tokens");
-
-      console.log("trying values on tokens");
-
-      console.log(tokens.values());
-      // Send notifications to all tokens.
-
-      return admin.messaging().sendToDevice(tokens, payload).then(response => {
-        // For each message check if there was an error.
-        const tokensToRemove = [];
-        response.results.forEach((result, index) => {
-          const error = result.error;
-          if (error) {
-            console.error('Failure sending notification to', tokens[index], error);
-            // Cleanup the tokens who are not registered anymore.
-             if (error.code === 'messaging/invalid-registration-token' ||
-                error.code === 'messaging/registration-token-not-registered') {
-              tokensToRemove.push(allTokens.ref.child(tokens[index]).remove());
-            }
-          }
-        });
-        return Promise.all(tokensToRemove);
-      });
-    }
-  });*/
 });
+//@params token ,topic 
+exports.unfollow = functions.https.onRequest((req, res) => {
+
+ var token = req.query.token;
+ console.log(token);
+ var topic =req.query.topic;
+ console.log(topic);
+
+
+    // Unsubscribe the device corresponding to the registration token from
+    // the topic.
+    admin.messaging().unsubscribeFromTopic(token, topic)
+    .then(function(response) {
+      console.log("Successfully unsubscribed from topic:", response);
+      res.status(200).json({ status: "success", data:response  });
+    })
+    .catch(function(error) {
+      console.log("Error unsubscribing from topic:", error);
+    });
+  })
+
+exports.follow=functions.https.onRequest((req,res)=>{
+  var token = req.query.token;
+  console.log(token);
+  var topic =req.query.topic;
+  console.log(topic);
+
+   
+    // Subscribe the device corresponding to the registration token to the
+    // topic.
+    admin.messaging().subscribeToTopic(token, topic)
+    .then(function(response) {
+      console.log("Successfully subscribed to topic:", response);
+      res.status(200).json({ status: "success", data:response  });
+    })
+    .catch(function(error) {
+      console.log("Error subscribing to topic:", error);
+    });
+
+  })
